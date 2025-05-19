dde00cbb8f77937a458b20bde085d560bea7d909
Kasey Crumpton <kasey.crumpton@kellogg.northwestern.edu>
2025-05-19 16:31:12 +0000

    Initial Route Created

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'places/index'
+  get 'places/show'
+  get 'places/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :places
+  
   # Defines the root path route ("/")
   # get("/", { :controller => "articles", :action => "index" })
 end
