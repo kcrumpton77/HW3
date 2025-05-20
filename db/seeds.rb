84f7806340ffffdee88912ed247f80aa7ef0042a
Kasey Crumpton <kasey.crumpton@kellogg.northwestern.edu>
2025-05-20 02:12:17 +0000

    Final Version

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,60 +10,81 @@ Entry.destroy_all
 Place.destroy_all
 
 brazil = Place.create(name: "Brazil")
-china = Place.create(name: "China")
-germany = Place.create(name: "Germany")
-peru = Place.create(name: "Peru")
 
 brazil.entries.create(
   title: "Cristo Statue",
   description: "Hiked and saw the statue",
-  occured_on: "2022-07-12"
+  occurred_on: "2022-07-12"
 )
 
 brazil.entries.create(
   title: "Ate Brazilian BBQ",
   description: "Brazilian Food is Amazing",
-  occured_on: "2022-07-13"
+  occurred_on: "2022-07-13"
 )
 
 brazil.entries.create(
   title: "Played Volleyball",
   description: "Copacobana Beach is the Best",
-  occured_on: "2022-07-14"
+  occurred_on: "2022-07-14"
 )
+
+china = Place.create(name: "China")
 
 china.entries.create(
   title: "Great Wall",
   description: "Hiked and Walked the Wall",
-  occured_on: "2021-07-12"
+  occurred_on: "2021-07-12"
 )
 
 china.entries.create(
   title: "Ate Chinese Food",
   description: "I Always Eat Too Much",
-  occured_on: "2021-07-13"
+  occurred_on: "2021-07-13"
 )
 
 china.entries.create(
   title: "Stuided Mandarin",
   description: "6 Week Study Abroad in China",
-  occured_on: "2021-07-14"
+  occurred_on: "2021-07-14"
 )
+
+germany = Place.create(name: "Germany")
 
 germany.entries.create(
   title: "Visited Berlin",
   description: "Saw All WWII Sites",
-  occured_on: "2023-07-12"
+  occurred_on: "2023-07-12"
 )
 
 germany.entries.create(
   title: "Traveled the Country",
   description: "Visted All Major Cities",
-  occured_on: "2023-07-13"
+  occurred_on: "2023-07-13"
 )
 
 germany.entries.create(
   title: "Moved to Germany",
   description: "Lived in Germany for 2 Years",
-  occured_on: "2023-07-14"
+  occurred_on: "2023-07-14"
+)
+
+peru = Place.create(name: "Peru")
+
+peru.entries.create(
+  title: "Machu Picchu",
+  description: "Saw The World Wonder",
+  occurred_on: "2024-07-12"
+)
+
+peru.entries.create(
+  title: "Traveled the Country",
+  description: "Visted Cusco and Lima",
+  occurred_on: "2024-07-13"
+)
+
+peru.entries.create(
+  title: "Ate Peruvian Food",
+  description: "Ceviche is soooo good",
+  occurred_on: "2024-07-14"
 )
